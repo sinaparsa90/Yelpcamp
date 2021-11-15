f64fb31e3a8dd77c437af5aa6d48a61ef648bbed
sinaparsa90 <82335614+sinaparsa90@users.noreply.github.com>
2021-11-15 18:45:09 +0330

    Add files via upload

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp_v3",{useNewUrlParser: true
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static( "public"));
+app.use(express.static(__dirname + "/public"));
+
 
 app.set("view engine", "ejs");
 
